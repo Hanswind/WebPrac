20c752f0cc9362a20a3ecf4b82b24efaf2c528a6
ChayanRin <k852012@naver.com>
2020-09-02 17:56:37 +0900

    리액트 13-1,2강. SPA 개념, 리액트 라우터 프로젝트 생성, BrowerRouter, Route, Link 태그 개념

---
+++ 리액트 프로젝트/TODOLIST/router-tutorial/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/about" component={About} />
+    </div>
+  );
+}
+
+export default App;
