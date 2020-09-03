eb8f5aa6a05f61ee249f8db449aecf739bbd7cee
ChayanRin <k852012@naver.com>
2020-09-03 18:05:22 +0900

    리액트 13.3, 4 여러 path에 한 route 로드하기 / URL 파라미터와 쿼리 처리하기

--- 리액트 프로젝트/TODOLIST/router-tutorial/src/App.js
+++ 리액트 프로젝트/TODOLIST/router-tutorial/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { Route, Link } from "react-router-dom";
 import About from "./About";
 import Home from "./Home";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -14,9 +15,16 @@ function App() {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profile/velopert">veloport 프로필</Link>
+        </li>
+        <li>
+          <Link to="/profile/gildong">gildong 프로필</Link>
+        </li>
       </ul>
       <Route path="/" component={Home} exact={true} />
       <Route path="/about" component={About} />
+      <Route path="/profile/:username" component={Profile} />
     </div>
   );
 }
